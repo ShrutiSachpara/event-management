91013ebf2c98bb3d44d5436cb37821713eb1216b
shruti sachpara <shrutiachpara.shivinfotech@gmail.com>
2024-01-09 17:51:07 +0530

    login page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BaseInputComponent } from './base/base-input/base-input.component';
+import { BaseButtonComponent } from './base/base-button/base-button.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, BaseInputComponent, BaseButtonComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  public loginForm: FormGroup;
+  public loading: boolean = false;
+  emailControl: FormControl<string | null>;
+  passwordControl: FormControl<string | null>;
+  // Create a FormGroup for the form controls
+  constructor(private fb: FormBuilder) {
+    this.emailControl = new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]);
+    this.passwordControl = new FormControl('', [
+      Validators.required,
+      // Validators.pattern('[(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}]'),
+    ]);
+
+    this.loginForm = this.fb.group({
+      email: this.emailControl,
+      password: this.passwordControl,
+    });
+  }
+
+  // Function to handle form submission
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    }
+  }
+}
