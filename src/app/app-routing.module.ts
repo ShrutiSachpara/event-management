247153abb0e3b96305f280dd6d47cac95f7afb84
shruti sachpara <shrutiachpara.shivinfotech@gmail.com>
2024-01-22 17:36:24 +0530

    making layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'verifyEmail', component: ForgotPasswordComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  {
+    path: 'dashboard',
+    component: LayoutComponent,
+    children: [{ path: '', component: DashboardComponent }],
+  },
 ];
 
 @NgModule({
