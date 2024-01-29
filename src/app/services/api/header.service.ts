9809051a93c83c444e4a085ae760c7c82db2131f
shruti sachpara <shrutiachpara.shivinfotech@gmail.com>
2024-01-29 16:31:16 +0530

    put header function

---
+++ src/app/services/api/header.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { take, switchMap } from 'rxjs/operators';
+import { selectAuthToken } from '../../auth/auth.selector';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeaderService {
+  constructor(private store: Store) {}
+
+  getRequestHeaders(): Observable<HttpHeaders> {
+    return this.store.pipe( 
+      select(selectAuthToken),
+      take(1),
+      switchMap((token) => {
+        const reqHeaders = new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+        });
+        return of(reqHeaders);
+      })
+    );
+  }
+}
--- src/app/services/api/manage-event.service.ts
+++ src/app/services/api/manage-event.service.ts
@@ -1,35 +1,19 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, switchMap, take } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { baseUrl } from './apiRoute';
-import { Store, select } from '@ngrx/store';
-import { AuthState } from 'src/app/auth/auth.reducer';
 import { url } from './apiUrl';
 import { AddEvent } from 'src/app/data-type';
-import { selectAuthToken } from '../../auth/auth.selector';
+import { HeaderService } from './header.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ManageEventService {
-  constructor(private http: HttpClient, private store: Store) {}
-
-  private getRequestHeaders(): Observable<HttpHeaders> {
-    return this.store.pipe(
-      select(selectAuthToken),
-      take(1),
-      switchMap((token) => {
-        const reqHeaders = new HttpHeaders({
-          'Content-Type': 'application/json',
-          Authorization: `${token}`,
-        });
-        return of(reqHeaders);
-      })
-    );
-  }
+  constructor(private http: HttpClient, private headerService: HeaderService) {}
 
   getEventData(): Observable<any> {
-    return this.getRequestHeaders().pipe(
+    return this.headerService.getRequestHeaders().pipe(
       switchMap((headers) => {
         const urls = baseUrl.basicUrl + url.listOfEvent;
         return this.http.post(urls, {}, { headers });
@@ -38,7 +22,7 @@ export class ManageEventService {
   }
 
   insertEventManageData(eventData: { event_name: any }): Observable<any> {
-    return this.getRequestHeaders().pipe(
+    return this.headerService.getRequestHeaders().pipe(
       switchMap((headers) => {
         const urls = baseUrl.basicUrl + url.addEvent;
         return this.http.post(urls, eventData, { headers });
@@ -47,7 +31,7 @@ export class ManageEventService {
   }
 
   updateEvent(id: number, eventData: AddEvent): Observable<any> {
-    return this.getRequestHeaders().pipe(
+    return this.headerService.getRequestHeaders().pipe(
       switchMap((headers) => {
         const urls = baseUrl.basicUrl + url.editEvent + id;
         return this.http.put(urls, eventData, { headers });
@@ -56,7 +40,7 @@ export class ManageEventService {
   }
 
   deleteEvent(id: number): Observable<any> {
-    return this.getRequestHeaders().pipe(
+    return this.headerService.getRequestHeaders().pipe(
       switchMap((headers) => {
         const urls = baseUrl.basicUrl + url.deleteEvent + id;
         let data = { is_deleted: true };
